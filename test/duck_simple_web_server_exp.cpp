011f0a43caed8324afd80d7bede8d0edc0c2b17b
kevin-nottberg <KN12599@gmail.com>
2021-03-02 14:13:59 -0700

    Update duck_simple_web_server_exp.cpp

--- test/duck_simple_web_server_exp.cpp
+++ test/duck_simple_web_server_exp.cpp
@@ -26,7 +26,7 @@ int main() {
     {
         printf("Attempting to connect to snottberg\n");
 
-        status = WiFi.begin("snottberg", "lilly1248");
+        status = WiFi.begin(//WIFI_SSID, //PASSWORD);
         sleep_ms(1000 * 10); // Wait 10 seconds for connection
     }
     server.begin();
